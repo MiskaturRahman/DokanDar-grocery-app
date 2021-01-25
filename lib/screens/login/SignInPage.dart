136e0bb4cb0c123dac9a14c7648f094a9f3f5caf
Misk007 <rahmnmisk007@gmail.com>
2021-01-25 20:22:28 +0600

    update

--- lib/screens/login/SignInPage.dart
+++ lib/screens/login/SignInPage.dart
@@ -1,8 +1,7 @@
-import 'package:dokandar_app/screens/login/SignUpPage.dart';
-import 'package:dokandar_app/shared/colors.dart';
-import 'package:dokandar_app/shared/styles.dart';
+import 'package:dokandar_app/screens/login/phoneAuth/numeric_pad.dart';
+import 'package:dokandar_app/screens/login/phoneAuth/verifty_phone.dart';
+
 import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
 
 class SignInPage extends StatefulWidget {
   final String pageTitle;
@@ -14,45 +13,160 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
+  String phoneNumber = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          leading: Icon(
+            Icons.close,
+            size: 30,
+            color: Colors.black,
+          ),
+          title: Text(
+            "Continue with phone",
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
+          ),
+          backgroundColor: Colors.white,
           elevation: 0,
-          backgroundColor: white,
-          title: Text('Sign In',
-              style: TextStyle(
-                  color: Colors.grey, fontFamily: 'Poppins', fontSize: 15)),
-          actions: <Widget>[
-            FlatButton(
-              onPressed: () {
-                // Navigator.of(context).pushReplacementNamed('/signup');
-                Navigator.pushReplacement(
-                    context,
-                    PageTransition(
-                        type: PageTransitionType.rightToLeft,
-                        child: SignUpPage()));
-              },
-              child: Text('Sign Up', style: contrastText),
-            )
-          ],
+          centerTitle: true,
+          textTheme: Theme.of(context).textTheme,
         ),
         body: new Stack(children: <Widget>[
           new Container(
               decoration: new BoxDecoration(
                   image: new DecorationImage(
-                      image: new AssetImage("images/9.png"),
+                      image: new AssetImage("assets/images/authpage.png"),
                       fit: BoxFit.cover)),
               child: Center(
-                child: Text(
-                  'Set Full Screen Background Image in Flutter',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.brown,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold),
-                ),
-              ))
+                  child: Column(
+                children: <Widget>[
+                  // Expanded(
+                  //   child: Container(
+                  //     // decoration: BoxDecoration(
+                  //     //   gradient: LinearGradient(
+                  //     //     begin: Alignment.topCenter,
+                  //     //     end: Alignment.bottomCenter,
+                  //     //     List: [
+                  //     //       Color(0xFFFFFFFF),
+                  //     //       Color(0xFFF7F7F7),
+                  //     //     ],
+                  //     //   ),
+                  //     // ),
+                  //     child: Column(
+                  //       mainAxisAlignment: MainAxisAlignment.center,
+                  //       children: <Widget>[
+                  //         SizedBox(
+                  //           height: 0,
+                  //           child:
+                  //               Image.asset('assets/images/holding-phone.png'),
+                  //         ),
+                  //         Padding(
+                  //           padding: EdgeInsets.symmetric(
+                  //               vertical: 14, horizontal: 64),
+                  //           child: Text(
+                  //             "You'll receive a 4 digit code to verify next.",
+                  //             style: TextStyle(
+                  //               fontSize: 22,
+                  //               color: Color(0xFF818181),
+                  //             ),
+                  //           ),
+                  //         ),
+                  //       ],
+                  //     ),
+                  //   ),
+                  // ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.13,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(25),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            width: 230,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  "Enter your phone",
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.grey,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  phoneNumber,
+                                  style: TextStyle(
+                                    fontSize: 24,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => VerifyPhone(
+                                          phoneNumber: phoneNumber)),
+                                );
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Color(0xFFFFDC3D),
+                                  borderRadius: BorderRadius.all(
+                                    Radius.circular(15),
+                                  ),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    "Continue",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  NumericPad(
+                    onNumberSelected: (value) {
+                      setState(() {
+                        if (value != -1) {
+                          phoneNumber = phoneNumber + value.toString();
+                        } else {
+                          phoneNumber =
+                              phoneNumber.substring(0, phoneNumber.length - 1);
+                        }
+                      });
+                    },
+                  ),
+                ],
+              )))
         ]));
   }
 }
